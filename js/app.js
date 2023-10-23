d51534a49fc97db1c7f80a3afd9f8c24cbf1dc72
DeboraChiarelli <deborachiarelli92@gmail.com>
2023-10-23 14:26:57 +0200

    Add - image in app.js

---
+++ js/app.js
@@ -0,0 +1,19 @@
+/*Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.*/
+
+const { createApp } = Vue;
+
+createApp({
+    
+data()
+{
+    return {
+        message: "Hello World",
+        color: "orange",
+        img: "img/DSC_0026_01.JPG",
+        imageContainer: "size",   
+    }  
+}
+
+}).mount("#container")
